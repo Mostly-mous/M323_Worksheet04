95fead63de1d025fd3b59a04b5f609d2331f4115
Mostly-mous <maurice.mattle@ict.csbe.ch>
2026-02-19 15:03:31 +0100

    functionalites implemented
    Added the js script for the page functionalities

---
+++ src/index.js
@@ -0,0 +1,42 @@
+/**
+ * this script contains the functionalities for the counter app
+ */
+
+//saving DOM-elements to variables
+const counterDisplay = document.getElementById('counter-display');
+const counterMinusButton = document.getElementById('subtract-button');
+const counterAddButton = document.getElementById('add-button');
+const counterButtons = document.getElementById('button-box');
+
+//creating variable for counter
+const counter = 0;
+
+//function to display counter
+function renderCounter() {
+    counterDisplay.textContent = counter;
+}
+
+renderCounter(counter);
+
+/**
+ * Eventlisteners for button actions
+ */
+counterAddButton.addEventListener('click', (e) => {
+    addCounter();
+});
+
+counterMinusButton.addEventListener('click', (e) => {
+    subtractCounter();
+});
+
+//function for increasesing counter
+function addCounter() {
+    counter++;
+    renderCounter(counter);
+}
+
+//function for decreasing counter
+function subtractCounter () {
+    counter--;
+    renderCounter(counter);
+}
